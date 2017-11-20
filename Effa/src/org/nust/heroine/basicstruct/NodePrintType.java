177eb0fe47c4bb58ea38da0deb156726b4c9347b
XiShanYongYe <18362965911@163.com>
2017-11-20 19:31:07 +0800

    Revert "effa"
    This reverts commit e1820b855ca7cd936e46d43f401a04fa303b9a1c.

--- Effa/src/org/nust/heroine/basicstruct/NodePrintType.java
+++
@@ -1,8 +0,0 @@
-package org.nust.heroine.basicstruct;
-
-/**
- * 节点类型 PLACE,TRANSITION,START,END,PARALLEL,CHOICE
- */
-public enum NodePrintType {
-	PLACE, TRANSITION, START, END, PARALLEL, CHOICE, BASIC
-}
